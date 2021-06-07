d595af9ff1e00e770e743d1f561a703b12f0d6da
0Supa <36031171+0Supa@users.noreply.github.com>
2021-06-08 00:41:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command)
 }
 
-client.knownCommands = ["8ball", "copypasta", "dadjoke", "donger", "fill", "%", "ping", "prefix", "pajbot", "pyramid", "title", "tts", "uptime", "weather", "yourmom", "notify", "help", "subemotes", "uid", "steam", "cat", "dog", "channels", "geoip", "query", "botinfo", "firstmsg", "randline", "chatters", "mostsent", "findmsg", "esearch", "avatar", "stalk", "math", "stats", "funfact", "suggest", "lines"]
+client.knownCommands = ["8ball", "copypasta", "dadjoke", "donger", "fill", "%", "ping", "prefix", "pajbot", "pyramid", "title", "tts", "uptime", "weather", "yourmom", "notify", "help", "subemotes", "uid", "steam", "cat", "dog", "channels", "geoip", "query", "botinfo", "firstmsg", "randline", "chatters", "mostsent", "findmsg", "esearch", "avatar", "stalk", "math", "stats", "funfact", "suggest", "lines", "everyone"]
 let cmdsJSON = []
 for (let cmdName of client.knownCommands) {
     let badgeURL;
