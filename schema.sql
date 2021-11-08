0d7c414a75a83f6dc4934589b3faccbface25551
Supa <36031171+0Supa@users.noreply.github.com>
2021-11-08 05:59:49 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,154 +1,171 @@
--- MySQL dump 10.17  Distrib 10.3.25-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: okey_bot
--- ------------------------------------------------------
--- Server version	10.3.25-MariaDB-0ubuntu0.20.04.1
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.3.31-MariaDB-0ubuntu0.20.04.1 - Ubuntu 20.04
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `bad_words`
---
-
-DROP TABLE IF EXISTS `bad_words`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bad_words` (
-  `word` varchar(500) NOT NULL
+-- Dumping structure for table okey_bot.7tv
+DROP TABLE IF EXISTS `7tv`;
+CREATE TABLE IF NOT EXISTS `7tv` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_7tv_channels` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `channels`
---
+-- Data exporting was unselected.
 
+-- Dumping structure for table okey_bot.channels
 DROP TABLE IF EXISTS `channels`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `channels` (
+CREATE TABLE IF NOT EXISTS `channels` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `platform_id` varchar(255) NOT NULL,
   `login` varchar(25) NOT NULL,
   `prefix` varchar(15) NOT NULL DEFAULT '?',
+  `pajbotAPI` varchar(500) DEFAULT NULL,
+  `logging` tinyint(4) NOT NULL DEFAULT 1,
   `added` timestamp NOT NULL DEFAULT current_timestamp(),
   `parted` tinyint(4) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`),
+  KEY `platform_id` (`platform_id`),
+  KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `data`
---
+-- Data exporting was unselected.
 
+-- Dumping structure for table okey_bot.confusables
+DROP TABLE IF EXISTS `confusables`;
+CREATE TABLE IF NOT EXISTS `confusables` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `char` varchar(50) NOT NULL DEFAULT '0',
+  `conf` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `char` (`char`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.data
 DROP TABLE IF EXISTS `data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `data` (
+CREATE TABLE IF NOT EXISTS `data` (
   `issued_commands` int(255) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `floroaia_jokes`
---
+-- Data exporting was unselected.
 
+-- Dumping structure for table okey_bot.emotes
+DROP TABLE IF EXISTS `emotes`;
+CREATE TABLE IF NOT EXISTS `emotes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `channel_login` varchar(25) NOT NULL,
+  `channel_id` varchar(255) NOT NULL,
+  `app_userid` varchar(500) DEFAULT NULL,
+  `emote_id` varchar(500) NOT NULL,
+  `reward_title` varchar(50) NOT NULL,
+  `app` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_emotes_channels` (`channel_id`),
+  CONSTRAINT `FK_emotes_channels` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`platform_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.errors
+DROP TABLE IF EXISTS `errors`;
+CREATE TABLE IF NOT EXISTS `errors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(500) NOT NULL DEFAULT '0',
+  `data` text DEFAULT NULL,
+  `error` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.floroaia_jokes
 DROP TABLE IF EXISTS `floroaia_jokes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `floroaia_jokes` (
+CREATE TABLE IF NOT EXISTS `floroaia_jokes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `joke` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `jake`
---
-
-DROP TABLE IF EXISTS `jake`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `jake` (
-  `emote_id` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `messages`
---
+-- Data exporting was unselected.
 
+-- Dumping structure for table okey_bot.hugs
+DROP TABLE IF EXISTS `hugs`;
+CREATE TABLE IF NOT EXISTS `hugs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.messages
 DROP TABLE IF EXISTS `messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `messages` (
+CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `channel_id` varchar(255) NOT NULL,
   `channel_login` varchar(25) NOT NULL,
   `user_id` varchar(255) NOT NULL,
   `user_login` varchar(25) NOT NULL,
-  `message` varchar(500) NOT NULL DEFAULT '',
+  `message` varchar(500) NOT NULL,
   `timestamp` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=674541 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`),
+  KEY `channel_id` (`channel_id`),
+  KEY `user_login` (`user_login`),
+  KEY `timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `notify`
---
+-- Data exporting was unselected.
 
+-- Dumping structure for table okey_bot.notify
 DROP TABLE IF EXISTS `notify`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `notify` (
+CREATE TABLE IF NOT EXISTS `notify` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `channel_id` varchar(255) NOT NULL,
   `channel_login` varchar(25) NOT NULL,
   `user_id` varchar(255) NOT NULL,
   `user_login` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`),
+  KEY `FK_notify_channels` (`channel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `notify_data`
---
+-- Data exporting was unselected.
 
+-- Dumping structure for table okey_bot.notify_data
 DROP TABLE IF EXISTS `notify_data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `notify_data` (
+CREATE TABLE IF NOT EXISTS `notify_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(255) NOT NULL,
   `login` varchar(25) NOT NULL,
-  `live` tinyint(4) DEFAULT 0,
-  `title` text DEFAULT NULL,
-  `category` text DEFAULT NULL,
+  `live` tinyint(4) NOT NULL DEFAULT 0,
   `online_format` varchar(350) NOT NULL,
   `offline_format` varchar(350) NOT NULL,
   `title_format` varchar(350) NOT NULL,
   `category_format` varchar(350) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `discord_webhook` varchar(500) DEFAULT NULL,
+  `discord_message` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_notify_data_channels` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `suggestions`
---
+-- Data exporting was unselected.
 
+-- Dumping structure for table okey_bot.suggestions
 DROP TABLE IF EXISTS `suggestions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `suggestions` (
+CREATE TABLE IF NOT EXISTS `suggestions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `author_login` varchar(25) NOT NULL,
   `author_id` varchar(255) NOT NULL,
@@ -158,16 +175,21 @@ CREATE TABLE `suggestions` (
   `created` timestamp NOT NULL DEFAULT current_timestamp(),
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.ttours
+DROP TABLE IF EXISTS `ttours`;
+CREATE TABLE IF NOT EXISTS `ttours` (
+  `author` varchar(500) NOT NULL,
+  `image` varchar(500) NOT NULL,
+  `notes` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-05-06 19:00:49
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
