4c5488fdbbfc6f79157b7ea7cbe3e1f88534cf40
Supa <36031171+0Supa@users.noreply.github.com>
2021-12-24 08:40:04 +0200

    small table name/structure changes

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.3.31-MariaDB-0ubuntu0.20.04.1 - Ubuntu 20.04
+-- Server version:               10.3.32-MariaDB-0ubuntu0.20.04.1 - Ubuntu 20.04
 -- Server OS:                    debian-linux-gnu
 -- HeidiSQL Version:             11.3.0.6295
 -- --------------------------------------------------------
@@ -12,19 +12,29 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping structure for table okey_bot.7tv
-DROP TABLE IF EXISTS `7tv`;
-CREATE TABLE IF NOT EXISTS `7tv` (
+
+-- Dumping database structure for okey_bot
+CREATE DATABASE IF NOT EXISTS `okey_bot` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `okey_bot`;
+
+-- Dumping structure for table okey_bot.7tv_updates
+CREATE TABLE IF NOT EXISTS `7tv_updates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_7tv_channels` (`login`)
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.bot_data
+CREATE TABLE IF NOT EXISTS `bot_data` (
+  `issued_commands` int(255) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table okey_bot.channels
-DROP TABLE IF EXISTS `channels`;
 CREATE TABLE IF NOT EXISTS `channels` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `platform_id` varchar(255) NOT NULL,
@@ -35,14 +45,13 @@ CREATE TABLE IF NOT EXISTS `channels` (
   `added` timestamp NOT NULL DEFAULT current_timestamp(),
   `parted` tinyint(4) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
-  KEY `platform_id` (`platform_id`),
-  KEY `login` (`login`)
+  UNIQUE KEY `platform_id` (`platform_id`),
+  UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table okey_bot.confusables
-DROP TABLE IF EXISTS `confusables`;
 CREATE TABLE IF NOT EXISTS `confusables` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `char` varchar(50) NOT NULL DEFAULT '0',
@@ -53,17 +62,8 @@ CREATE TABLE IF NOT EXISTS `confusables` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table okey_bot.data
-DROP TABLE IF EXISTS `data`;
-CREATE TABLE IF NOT EXISTS `data` (
-  `issued_commands` int(255) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table okey_bot.emotes
-DROP TABLE IF EXISTS `emotes`;
-CREATE TABLE IF NOT EXISTS `emotes` (
+-- Dumping structure for table okey_bot.emote_rewards
+CREATE TABLE IF NOT EXISTS `emote_rewards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `channel_login` varchar(25) NOT NULL,
   `channel_id` varchar(255) NOT NULL,
@@ -79,7 +79,6 @@ CREATE TABLE IF NOT EXISTS `emotes` (
 -- Data exporting was unselected.
 
 -- Dumping structure for table okey_bot.errors
-DROP TABLE IF EXISTS `errors`;
 CREATE TABLE IF NOT EXISTS `errors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(500) NOT NULL DEFAULT '0',
@@ -91,29 +90,29 @@ CREATE TABLE IF NOT EXISTS `errors` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table okey_bot.floroaia_jokes
-DROP TABLE IF EXISTS `floroaia_jokes`;
-CREATE TABLE IF NOT EXISTS `floroaia_jokes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `joke` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
 -- Dumping structure for table okey_bot.hugs
-DROP TABLE IF EXISTS `hugs`;
 CREATE TABLE IF NOT EXISTS `hugs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(255) NOT NULL,
   `count` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.ignored_users
+CREATE TABLE IF NOT EXISTS `ignored_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) NOT NULL,
+  `reason` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table okey_bot.messages
-DROP TABLE IF EXISTS `messages`;
 CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `channel_id` varchar(255) NOT NULL,
@@ -130,23 +129,8 @@ CREATE TABLE IF NOT EXISTS `messages` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table okey_bot.notify
-DROP TABLE IF EXISTS `notify`;
-CREATE TABLE IF NOT EXISTS `notify` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `channel_id` varchar(255) NOT NULL,
-  `channel_login` varchar(25) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
-  `user_login` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_notify_channels` (`channel_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table okey_bot.notify_data
-DROP TABLE IF EXISTS `notify_data`;
-CREATE TABLE IF NOT EXISTS `notify_data` (
+-- Dumping structure for table okey_bot.notify_channels
+CREATE TABLE IF NOT EXISTS `notify_channels` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(255) NOT NULL,
   `login` varchar(25) NOT NULL,
@@ -158,13 +142,25 @@ CREATE TABLE IF NOT EXISTS `notify_data` (
   `discord_webhook` varchar(500) DEFAULT NULL,
   `discord_message` varchar(2000) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_notify_data_channels` (`user_id`)
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table okey_bot.notify_users
+CREATE TABLE IF NOT EXISTS `notify_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `channel_id` varchar(255) NOT NULL,
+  `channel_login` varchar(25) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `user_login` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_notify_channels` (`channel_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table okey_bot.suggestions
-DROP TABLE IF EXISTS `suggestions`;
 CREATE TABLE IF NOT EXISTS `suggestions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `author_login` varchar(25) NOT NULL,
@@ -174,17 +170,10 @@ CREATE TABLE IF NOT EXISTS `suggestions` (
   `notes` varchar(2000) DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT current_timestamp(),
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table okey_bot.ttours
-DROP TABLE IF EXISTS `ttours`;
-CREATE TABLE IF NOT EXISTS `ttours` (
-  `author` varchar(500) NOT NULL,
-  `image` varchar(500) NOT NULL,
-  `notes` varchar(500) NOT NULL
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  KEY `created` (`created`),
+  KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
