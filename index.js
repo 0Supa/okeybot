2c34927b511759bf7b4c137f95b336b6941677ff
Supa <36031171+0Supa@users.noreply.github.com>
2022-05-27 16:28:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -170,6 +170,7 @@ client.on("PRIVMSG", async (msg) => {
     handle(msgData)
     if (msgData.user.login === 'pajbot' && msgData.text === 'pajaS 🚨 ALERT') msgData.send('dankS 🚨')
     if (msgData.channel.login === 'supinic' && msgData.user.login === 'supibot' && msgData.text === 'ppCircle') msgData.send('ppAutismo')
+    if (msgData.channel.login === 'aliengathering' && /!shoutout\b/i.test(msgData.text)) msgData.send(`Pepega 📣 @${msgData.user.login}`)
 })
 
 client.on('WHISPER', async (msg) => {
