1e178257c4d10408ad3ea581160ce7274be2353b
Supa <36031171+0Supa@users.noreply.github.com>
2022-11-26 17:46:24 +0200

    Update Home.svelte

--- web/src/routes/Home.svelte
+++ web/src/routes/Home.svelte
@@ -5,12 +5,12 @@
 <div class="container">
     <h1>Description</h1>
     <blockquote>
-        <p>Okeybot provides a variety of fun and utility commands<br />If you have any questions or suggestions you can use the <b>?suggest</b> command</p>
+        <p>Okeybot provides a variety of fun and utility commands<br />If you have any questions or suggestions, you can use the <code>?suggest</code> command</p>
     </blockquote>
 
     <h1>Usage</h1>
     <blockquote>
-        <p>The default command prefix is "<b>?</b>"<br />The prefix can be changed per channel by using the <b>?prefix</b> command<br />You can run commands by typing the prefix followed by the command name, for example you would run the ping command by typing <b>?ping</b></p>
+        <p>The default command prefix is <code>"<b>?</b>"</code><br />This prefix can be changed per channel by using the <code>?prefix</code> command<br />You can run commands by typing the prefix followed by the command name, for example you would run the ping command by typing <code>?ping</code></p>
         <p>Some commands have pre-defined user cooldowns to prevent spam</p>
     </blockquote>
 
@@ -18,8 +18,8 @@
     <blockquote>
         <p>Use the following commands if you want Okeybot added in a channel</p>
         <ul>
-            <li><b>?addbot</b> for adding the bot in your chat</li>
-            <li><b>?addbot <u>(channel name)</u></b> for adding the bot in a chat that you moderate</li>
+            <li><code>?addbot</code> for adding the bot in your chat</li>
+            <li><code>?addbot <u>(channel name)</u></code> for adding the bot in a chat that you moderate</li>
         </ul>
         <p>The bot will automatically rejoin after a name change or a Twitch suspension, this process will take up to 30 minutes</p>
         <p>If you want the bot removed from your channel, you can just ban it <img class="emote" alt="4Head" src="4Head.png" /></p>
@@ -47,6 +47,12 @@
         padding: 0 10px;
         border-left: 2px solid gray;
         border-radius: 10px;
+    }
+
+    code {
+        background: #191919;
+        padding: 2px;
+        border-radius: 5px;
     }
 
     .emote {
